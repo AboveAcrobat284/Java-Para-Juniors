0e44c852299d3e1fe55290d3b548f28e5f8893ec
AboveAcrobat284 <cgumetanavarro@gmail.com>
2023-03-02 07:58:37 -0600

    CorrectionRegister

--- src/components/atoms/Picture.jsx
+++ src/components/atoms/Picture.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const StyledImg = styled.img`
     height: ${props => props.height ? '141px' : '221px'};
-    width: ${props => props.width ? '170px' : '338px'};
+    width: ${props => props.width ? '160px' : '338px'};
 `;
 
 function Picture({src, height, width}) {
---
+++ src/pages/Register.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import '../assets/styles/Login.css'
+import FormRegister from "../components/organism/FormRegister";
+
+function Login() {
+  return (
+    <>
+    <FormRegister></FormRegister>
+    </>
+  );
+}
+export default Login;
